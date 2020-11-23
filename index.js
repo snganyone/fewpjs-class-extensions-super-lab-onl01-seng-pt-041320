9ad80253b6a176bc019c8e2db6fa07f97874c73d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-23 13:58:02 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your classes here
+
+class Tree{
+    constructor(species){
+        this.species = species;
+    }
+
+    static definition(){
+        return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.';
+    }
+}
+
+let tree = new Tree('apple');
+console.log(tree.species);
